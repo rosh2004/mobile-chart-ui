f04b68485fbb41669b13e79f1ffd1638561dedf6
rosh20043 <roshaan20043@gmail.com>
2025-08-17 01:37:37 +0500

    made select underline responsive

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { Header } from "./components/header/header";
 import { DurationSelect } from "./components/duration-select/duration-select";
+import { BarChart } from "./components/charts/bar-chart/bar-chart";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [Header, DurationSelect]
+  imports: [Header, DurationSelect, BarChart]
 })
 export class App {
   protected readonly title = signal('chart-ui-app');
---
+++ src/app/components/charts/bar-chart/bar-chart.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bar-chart',
+  imports: [],
+  templateUrl: './bar-chart.html',
+  styleUrl: './bar-chart.scss'
+})
+export class BarChart {
+
+}
